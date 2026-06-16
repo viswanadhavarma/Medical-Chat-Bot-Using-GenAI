8676b2a01a9339a806be70cdb08f64f9c655dd6c
Viswanadha Varma Dantuluri <96356470+viswanadhavarma@users.noreply.github.com>
2026-06-16 18:51:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_embedding
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.prompts import ChatPromptTemplate
+from src.prompt import *
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+embeddings = download_embedding()
+
+index_name = "med"
+
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name = index_name,
+    embedding=embeddings
+
+)
+
+retriever = docsearch.as_retriever(search_type="similarity",search_kwargs={"k":3})
+
+llm = OpenAI(temperature=0.4,max_tokens=500)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human","{input}")
+    ]
+)
+
+ques_ans_gen = create_stuff_documents_chain(llm, prompt)
+rag_chain = create_retrieval_chain(retriever, ques_ans_gen)
+
+@app.route("/")
+def index():
+    return render_template("chat.html")
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input":msg})
+    print("Response : ",response["answer"])
+    return str(response["answer"])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port= 8080, debug= True)
